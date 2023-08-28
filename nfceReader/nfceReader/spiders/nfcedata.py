30a2a7be19777b30173da87f6cfa77305b224447
Matheus Valentim <meduardo.valentim@gmail.com>
2023-08-27 23:06:04 -0300

    feat: create spider for collect data from a specific url

--- nfceReader/nfceReader/spiders/nfcedata.py
+++ nfceReader/nfceReader/spiders/nfcedata.py
@@ -1,10 +1,18 @@
 import scrapy
 
-
 class NfcedataSpider(scrapy.Spider):
     name = "nfcedata"
     allowed_domains = ["www.fazenda.pr.gov.br"]
-    start_urls = ["https://www.fazenda.pr.gov.br"]
+    start_urls = ["http://www.fazenda.pr.gov.br/nfce/qrcode?p=41230801438784001098650080002284341220330512|2|1|1|f730aa13ef866b8f09342adbf4c545ea001e4dd5"]
 
     def parse(self, response):
-        pass
+        names = response.css("td span.txtTit2::text").getall()
+        valores = response.css("td span.valor::text").getall()
+
+        for name, valor in zip(names, valores):
+            data = {
+                'name': name,
+                'valor': valor,
+            }
+
+            yield data
