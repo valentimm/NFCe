45e0bc32f93c02fdaff523a6aded02b33d74f0c7
Matheus <meduardo.valentim@gmail.com>
2024-06-17 12:37:10 -0300

    feat: changed spider

--- nfceReader/nfceReader/spiders/nfcedata.py
+++ nfceReader/nfceReader/spiders/nfcedata.py
@@ -4,26 +4,26 @@ class NfcedataSpider(scrapy.Spider):
     name = "nfcedata"
     allowed_domains = ["www.fazenda.pr.gov.br"]
 
+    # Configurações personalizadas
     custom_settings = {
         'FEEDS': {
-            'products.json': {'format': 'json'},
+            '../../../Items.csv': {'format': 'csv'},
         }
     }
     
     def start_requests(self):
         url = getattr(self, 'url', None)
         if url:
-            yield scrapy.Request(url, self.parse)
+            yield scrapy.Request(url, callback=self.parse)
 
     def parse(self, response):
+        # Extrai os dados da página
         names = response.css("td span.txtTit2::text").getall()
         valores = response.css("td span.valor::text").getall()
 
+        # Itera pelos dados e gera os itens
         for name, valor in zip(names, valores):
-            data = {
+            yield {
                 'name': name,
                 'valor': valor,
             }
-
-            yield data
-
